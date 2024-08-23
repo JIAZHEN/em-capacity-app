f6a725a38ba3e19452e363836a0b44e383589f29
Jiazhen Xie <jiazhen.xie@gousto.co.uk>
2024-08-23 23:27:09 +0100

    Build model for capacity

---
+++ app/models/business_day.rb
@@ -0,0 +1,39 @@
+class BusinessDay
+  attr_reader :months, :bank_holidays, :business_days
+
+  def initialize(bank_holidays)
+    @bank_holidays = bank_holidays
+    self.get_monthly_business_days()
+  end
+
+  def get_monthly_business_days()
+    the_year = Date.current.year
+    @business_days ||= (1..12).reduce({}) do |result, month|
+      start_date = Date.new(the_year, month, 1)
+      end_date = start_date.end_of_month
+
+      dates = (start_date..end_date).filter {|d| self.business_day?(d) }
+      result[start_date.strftime("%B")] = dates.size
+      result
+    end
+  end
+
+  def business_days_in_month(month_name)
+    business_days = get_monthly_business_days()
+    business_days[month_name]
+  end
+
+  def get_employee_business_days(employee, month_name)
+    business_days_in_month(month_name) - employee.absence_days_in_month(month_name)
+  end
+
+  private
+
+  def business_day?(date)
+    working_day?(date) && !self.bank_holidays.include?(date)
+  end
+
+  def working_day?(date)
+    !date.saturday? && !date.sunday?
+  end
+end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,18 @@
 class Employee < ApplicationRecord
+  has_many :employee_factors
+  has_many :absences
+
+  def absences_between(start_date, end_date)
+    self.absences.where(calendar_date: start_date..end_date)
+  end
+
+  def absence_days_in_month(month_number)
+    the_year = Date.current.year
+    start_date = Date.new(the_year, month_number, 1)
+    end_date = start_date.end_of_month
+
+    self.absences_between(start_date, end_date).reduce(0) do |result, absence|
+      absence.half_day ? result + 0.5 : result + 1
+    end
+  end
 end
