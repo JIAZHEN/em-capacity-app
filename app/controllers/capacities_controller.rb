f06e8ab87cdc59ae1afa7a14c038d5a67f2c9b78
Jiazhen Xie <jiazhen.xie@gousto.co.uk>
2024-08-29 23:31:08 +0100

    Simplify

--- app/controllers/capacities_controller.rb
+++ app/controllers/capacities_controller.rb
@@ -5,20 +5,24 @@ class CapacitiesController < ApplicationController
     the_year = Date.current.year
     @current_month = Date.current.month
     @month_names = MONTH_NAMES
-    @bank_holidays = BankHoliday.where(year: the_year)
+    @bank_holidays = BankHoliday.where(year: the_year).map(&:calendar_date)
     @employees = Employee.includes(:employee_factors, :employee_allowances, :absences).all
     capacity_calculator = CapacityCalculator.new(@bank_holidays)
+
+    dates = (1..12).map do |month|
+      start_date = Date.new(the_year, month, 1)
+      [start_date, start_date.end_of_month] 
+    end
 
     @employees_engineering_days = []
     @holiday_remainings = 0
     @employees.map do |employee|
       employee_engineering_days = [employee.name]
-      @month_names.map do |month_name|
-        engineering_days = capacity_calculator.get_employee_engineering_days(employee, month_name)
-        # get the sum of the employee row
-        month_index = capacity_calculator.get_month_number_from(month_name)
+      dates.each do |start_date, end_date|
+        engineering_days = capacity_calculator.get_employee_engineering_days_between(employee, start_date, end_date)
         employee_engineering_days << engineering_days
       end
+      
       @holiday_remainings += employee.holiday_remaining_in_year(the_year)
       @employees_engineering_days << employee_engineering_days
     end
--- app/models/capacity_calculator.rb
+++ app/models/capacity_calculator.rb
@@ -5,61 +5,24 @@ class CapacityCalculator
 
   def initialize(bank_holidays)
     @bank_holidays = bank_holidays
-    self.get_monthly_business_days()
-  end
-
-  def get_monthly_business_days()
-    the_year = Date.current.year
-    @business_days ||= (1..12).reduce({}) do |result, month|
-      start_date = Date.new(the_year, month, 1)
-      end_date = start_date.end_of_month
-      dates = self.business_days_between(start_date, end_date)
-      month_name = MONTH_NAMES[month - 1]
-      result[month_name] = dates.size
-      result
-    end
-  end
-
-  def business_days_in_month(month_name)
-    business_days = get_monthly_business_days()
-    business_days[month_name]
-  end
-
-  def get_employee_business_days(employee, month_name)
-    month_number = self.get_month_number_from(month_name)
-    business_days_in_month(month_name) - employee.absence_days_in_month(month_number)
-  end
-
-  def get_employee_engineering_days(employee, month_name)
-    pure_business_days = get_employee_business_days(employee, month_name)
-    month_number = self.get_month_number_from(month_name)
-    engineering_factor = employee.engineering_factor_in_month(month_number)
-    unless engineering_factor
-      raise "No engineering factor found for #{employee.name} in #{month_name}"
-    end
-
-    pure_business_days * engineering_factor
   end
 
   def get_employee_engineering_days_between(employee, start_date, end_date)
     employee_factors = employee.engineering_factors_between(start_date, end_date)
     absences = employee.absences_between(start_date, end_date)
-
     (start_date..end_date).reduce(0) do |result, d|
-      return result unless business_day?(d)
-      factor = employee_factors["#{d.year}-#{d.month}"]
-      unless factor
-        raise "No engineering factor found for #{employee.name} in #{d.year}-#{d.month}}"
+      if business_day?(d)
+        factor = employee_factors["#{d.year}-#{d.month}"]
+        unless factor
+          raise "No engineering factor found for #{employee.name} in #{d.year}-#{d.month}}"
+        end
+        found_absence = absences.find { |absence| absence.calendar_date == d }
+        day = found_absence ? (found_absence.half_day ? 0.5 : 0) : 1
+        result += factor * day
       end
 
-      found_absence = absences.find { |absence| absence.calendar_date == d }
-      day = found_absence ? (found_absence.half_day ? 0.5 : 0) : 1
-      result + factor * day
+      result
     end
-  end
-
-  def get_month_number_from(month_name)
-    MONTH_NAMES.find_index(month_name) + 1
   end
 
   # employees_engineering_days is an array of monthly employee's engineering days
@@ -88,9 +51,5 @@ class CapacityCalculator
 
   def working_day?(date)
     !date.saturday? && !date.sunday?
-  end
-
-  def business_days_between(start_date, end_date)
-    (start_date..end_date).filter {|d| business_day?(d) }
   end
 end
