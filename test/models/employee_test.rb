5173cdd3a89e002a3782bb8f51eff5011641b20b
Jiazhen Xie <jiazhen.xie@gousto.co.uk>
2024-08-31 17:33:11 +0100

    Adding unit test for models

--- test/models/employee_test.rb
+++ test/models/employee_test.rb
@@ -13,9 +13,34 @@ class EmployeeTest < ActiveSupport::TestCase
     assert_equal 2.0, days
   end
 
+  test "engineering_factors_between returns empty if not found" do
+    employee = employees(:one)
+    factors = employee.engineering_factors_between(Date.new(2024, 1, 1), Date.new(2024, 1, 31))
+    assert_equal({}, factors)
+  end
+
+  test "engineering_factors_between returns map if found" do
+    employee = employees(:one)
+    factors = employee.engineering_factors_between(Date.new(2014, 1, 1), Date.new(2014, 3, 31))
+    assert_equal({ "2014-1" => 1.5 }, factors)
+  end
+
   test "allowance_in_year returns nil if no allowance found" do
     employee = employees(:one)
     allowance = employee.allowance_in_year(2022)
     assert_nil allowance
+  end
+
+  test "allowance_in_year returns 30 if allowance is found" do
+    employee = employees(:one)
+    allowance = employee.allowance_in_year(2024)
+    assert_equal 30, allowance.holiday_allowance
+    assert_equal 5, allowance.sick_leave_allowance
+  end
+
+  test "holiday_remaining_in_year returns right result when person has 1.5 out of 30 holidays" do
+    employee = employees(:one)
+    days = employee.holiday_remaining_in_year(2024)
+    assert_equal 28.5, days
   end
 end
