6fcdc69c41ce40df6991dcb86f5e3fdf4fd1f8d0
Jiazhen Xie <jiazhen@canva.com>
2024-09-27 22:37:56 +0100

    Upgrade to 7.2

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module EmCapacityApp
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -23,7 +23,5 @@ module EmCapacityApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.assets.css_compressor = nil
-    config.time_zone = "London"
   end
 end
